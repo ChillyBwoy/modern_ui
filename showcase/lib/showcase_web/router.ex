33c3125866dd040ca2a9e6a855882d7f7b3b1bea
Eugene Cheltsov <chill.icp@gmail.com>
2025-09-01 22:50:44 +0900

    chore: add sb app

---
+++ showcase/lib/showcase_web/router.ex
@@ -0,0 +1,12 @@
+defmodule ShowcaseWeb.Router do
+  use ShowcaseWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {ShowcaseWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+end
